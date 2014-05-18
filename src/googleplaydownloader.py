430f52e14aceb51ac9363784a569347d4de7b164
Tuxicoman <Tuxicoman@3d3ae53f-1415-4b70-8da7-8e051a50b823>
2014-05-18 18:25:33 +0000

    more robust config file

--- src/googleplaydownloader.py
+++ src/googleplaydownloader.py
@@ -42,20 +42,25 @@ config_section = "googleplaydownloader"
 def read_config(config_file_path, config_dict):
   configparser = ConfigParser.RawConfigParser()
   configparser.read(config_file_path)
-  for key in config_dict.keys():
+  for key, previous_value in config_dict.items():
     if configparser.has_option(config_section, key):
-      config_dict[key] = configparser.get(config_section, key).decode('utf-8')
+      new_value = configparser.get(config_section, key).decode('utf-8')
+      if type(previous_value) in (list, bool):
+        new_value = json.loads(new_value)
+      config_dict[key] = new_value
 
 def save_config(config_file_path, config_dict):
   configparser = ConfigParser.RawConfigParser()
   configparser.add_section(config_section)
   for key, value in config_dict.items():
+    if type(value) in (list, bool) :
+      value = json.dumps(value)
     configparser.set(config_section, key, value.encode('utf-8'))
     
   config_file_folder = os.path.dirname(config_file_path)
   if not os.path.exists(config_file_folder) :
     os.makedirs(config_file_folder)
-  with open(config_file_path, 'wb') as configfile:
+  with open(config_file_path, 'w') as configfile:
     configparser.write(configfile)
   
 def sizeof_fmt(num):
