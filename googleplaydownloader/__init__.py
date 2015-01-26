2fcbd7c15be44178711219876a98772cf39eea92
Tuxicoman <Tuxicoman@3d3ae53f-1415-4b70-8da7-8e051a50b823>
2015-01-26 23:29:51 +0000

    version 1.7

--- googleplaydownloader/__init__.py
+++ googleplaydownloader/__init__.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from .version import __version__
+import os
+HERE = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(HERE, "googleplaydownloader", "version.txt"), "r") as f:
+  __version__ = f.read().strip()
 
 from .googleplaydownloader import main as start_gui
 
